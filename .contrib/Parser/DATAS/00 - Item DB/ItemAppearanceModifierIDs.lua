ad98da41ec7987385cbbe49341a3806f8a534630
Crieve <dylanfortune@live.com>
2025-04-30 03:01:34 -0700

    Updated the modifierIDs tables.

--- .contrib/Parser/DATAS/00 - Item DB/ItemAppearanceModifierIDs.lua
+++ .contrib/Parser/DATAS/00 - Item DB/ItemAppearanceModifierIDs.lua
@@ -4,28 +4,46 @@
 root("ItemAppearanceModifierIDs_BonusID", {
 	[1] = 1,
 	[450] = 3,
+	[525] = 6,
 	[4746] = 5,		-- ??
+	[5842] = 3,		-- ??
+	[6272] = 150,	-- ??
+	[6277] = 151,	-- ??
+	[6283] = 152,	-- ??
+	[6376] = 3,		-- ??
 	[6894] = 153,	-- ??
 	[6895] = 154,	-- ??
 	[6896] = 155,	-- ??
 	[6897] = 156,	-- ??
 	[6898] = 157,	-- Gladiator
 	[8107] = 158,	-- Elite Gladiator
+	[7309] = 3,		-- ??
 	[7479] = 159,	-- ??
 	[7532] = 160,	-- PvP Elite Gladiator
+	[7551] = 160,	-- ??
+	[7720] = 153,	-- ??
+	[7723] = 154,	-- ??
+	[7724] = 155,	-- ??
+	[7725] = 156,	-- ??
 	[7897] = 159,	-- Gladiator??
+	[9145] = 7,
+	[9146] = 8,
+	[9344] = 1,		-- ??
 });
 root("ItemAppearanceModifierIDs_ModID", {
 	[2] = 1,		-- DIFFICULTY.DUNGEON.HEROIC
-	[3] = 1,
+	[3] = 0,
 	[4] = 4,		-- DIFFICULTY.RAID.LFR
-	[5] = 3,		-- DIFFICULTY.RAID.HEROIC
+	[5] = 1,		-- DIFFICULTY.RAID.HEROIC
 	[6] = 3,		-- DIFFICULTY.RAID.MYTHIC
 	[8] = 159,
+	[14] = 159,
 	[22] = 0,		-- DIFFICULTY.DUNGEON.TIMEWALKING
-	[23] = 4,		-- DIFFICULTY.DUNGEON.MYTHIC
+	[23] = 3,		-- DIFFICULTY.DUNGEON.MYTHIC
 	[28] = 154,		-- ??
 	[33] = 1,
 	[35] = 1,
+	[45] = 3,
+	[85] = 3,
 	[162] = 3,
 });
