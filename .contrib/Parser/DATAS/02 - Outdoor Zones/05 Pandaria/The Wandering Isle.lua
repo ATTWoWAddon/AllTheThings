aaf4738eb43ab736a7db08e743099d2bc093ea49
Chun-Chi Hung <b10102229@gmail.com>
2025-05-28 16:54:06 +0800

    [DB] Mist: Update Wandering Isle.
    Add class and race info.

--- .contrib/Parser/DATAS/02 - Outdoor Zones/05 Pandaria/The Wandering Isle.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/05 Pandaria/The Wandering Isle.lua
@@ -167,13 +167,34 @@ root(ROOTS.Zones, {
 						["sourceQuests"] = { 29775 },	-- The Spirit and Body of Shen-zin Su
 					}),
 					un(REMOVED_FROM_GAME, q(29404)),	-- Much to Learn (NYI?)
-					un(REMOVED_FROM_GAME, q(30039)),	-- Much to Learn (Monk)
-					un(REMOVED_FROM_GAME, q(30040)),	-- Much to Learn (Mage)
-					un(REMOVED_FROM_GAME, q(30041)),	-- Much to Learn (Hunter)
-					un(REMOVED_FROM_GAME, q(30042)),	-- Much to Learn (Priest)
-					un(REMOVED_FROM_GAME, q(30043)),	-- Much to Learn (Rouge)
-					un(REMOVED_FROM_GAME, q(30044)),	-- Much to Learn (Shaman)
-					un(REMOVED_FROM_GAME, q(30045)),	-- Much to Learn (Warrior)
+					q(30039, {	-- Much to Learn (Monk)
+						["classes"] = { MONK },
+						["races"] = { PANDAREN_NEUTRAL, PANDAREN_ALLIANCE, PANDAREN_HORDE },
+					}),
+					q(30039, {	-- Much to Learn (Mage)
+						["classes"] = { MAGE },
+						["races"] = { PANDAREN_NEUTRAL, PANDAREN_ALLIANCE, PANDAREN_HORDE },
+					}),
+					q(30039, {	-- Much to Learn (Hunter)
+						["classes"] = { HUNTER },
+						["races"] = { PANDAREN_NEUTRAL, PANDAREN_ALLIANCE, PANDAREN_HORDE },
+					}),
+					q(30039, {	-- Much to Learn (Priest)
+						["classes"] = { PRIEST },
+						["races"] = { PANDAREN_NEUTRAL, PANDAREN_ALLIANCE, PANDAREN_HORDE },
+					}),
+					q(30039, {	-- Much to Learn (Rogue)
+						["classes"] = { ROGUE },
+						["races"] = { PANDAREN_NEUTRAL, PANDAREN_ALLIANCE, PANDAREN_HORDE },
+					}),
+					q(30039, {	-- Much to Learn (Shaman)
+						["classes"] = { SHAMAN },
+						["races"] = { PANDAREN_NEUTRAL, PANDAREN_ALLIANCE, PANDAREN_HORDE },
+					}),
+					q(30039, {	-- Much to Learn (Warrior)
+						["classes"] = { WARRIOR },
+						["races"] = { PANDAREN_NEUTRAL, PANDAREN_ALLIANCE, PANDAREN_HORDE },
+					}),
 					q(29800, {	-- New Allies
 						["provider"] = { "n", 57739 },	-- Ji Firepaw
 						["sourceQuests"] = { 29799 },	-- The Healing of Shen-zin Su
