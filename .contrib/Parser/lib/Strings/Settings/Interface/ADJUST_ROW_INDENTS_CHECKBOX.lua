a7fb849c649b9a28ba4e8012863b3f3ffce7d604
Crieve <dylanfortune@live.com>
2026-01-22 20:40:26 -0600

    Added localization for the new settings menu elements.

---
+++ .contrib/Parser/lib/Strings/Settings/Interface/ADJUST_ROW_INDENTS_CHECKBOX.lua
@@ -0,0 +1,18 @@
+createLocalizationString({
+	readable = "Adjust Row Indents",
+	constant = "ADJUST_ROW_INDENTS_CHECKBOX",
+	export = true,
+	text = {
+		en = "Adjust Row Indents",
+		-- TODO: de = "",
+		-- TODO: es = "",
+		-- TODO: mx = "",
+		-- TODO: fr = "",
+		-- TODO: it = "",
+		-- TODO: pt = "",
+		-- TODO: ru = "",
+		-- TODO: ko = "",
+		-- TODO: cn = "",
+		-- TODO: tw = "",
+	},
+})
---
+++ .contrib/Parser/lib/Strings/Settings/Interface/INACTIVE_WINDOW_ALPHA_TOOLTIP.lua
@@ -0,0 +1,18 @@
+createLocalizationString({
+	readable = "Set an alpha to use for inactive windows.\n\nDefault: 1",
+	constant = "INACTIVE_WINDOW_ALPHA_TOOLTIP",
+	export = true,
+	text = {
+		en = "Set an alpha to use for inactive windows.\n\nDefault: 1",
+		-- TODO: de = "",
+		-- TODO: es = "",
+		-- TODO: mx = "",
+		-- TODO: fr = "",
+		-- TODO: it = "",
+		-- TODO: pt = "",
+		-- TODO: ru = "",
+		-- TODO: ko = "",
+		-- TODO: cn = "",
+		-- TODO: tw = "",
+	},
+})
