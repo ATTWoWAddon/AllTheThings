f750a4daf3e83cebcfba0bfef4b7bcf4ef1eaa1c
Crieve <dylanfortune@live.com>
2026-01-26 00:07:31 -0600

    The Mini List now supports the Retail style from before the chaos began, though something seems wrong.

--- src/UI/Windows/Mini List.lua
+++ src/UI/Windows/Mini List.lua
@@ -2,17 +2,16 @@
 local _, app = ...;
 local L = app.L;
 
-if app.IsRetail then return; end
-
-local contains, containsValue = app.contains, app.containsValue;
-local ExpandGroupsRecursively, GetRelativeValue, MergeObject
-	= app.ExpandGroupsRecursively, app.GetRelativeValue, app.MergeObject;
-
 -- Global locals
-local ipairs, pairs, tinsert, math_floor, getmetatable, setmetatable, tostring =
-	  ipairs, pairs, tinsert, math.floor, getmetatable, setmetatable, tostring;
-local C_Map_GetMapInfo, C_Map_GetPlayerMapPosition
-	= C_Map.GetMapInfo, C_Map.GetPlayerMapPosition;
+local ipairs, pairs, getmetatable, setmetatable, tostring, tinsert, math_floor, wipe
+	= ipairs, pairs, getmetatable, setmetatable, tostring, tinsert, math.floor, wipe;
+local C_Map_GetMapInfo, C_Map_GetPlayerMapPosition, GetRealZoneText, GetSubZoneText, IsInInstance
+	= C_Map.GetMapInfo, C_Map.GetPlayerMapPosition, GetRealZoneText, GetSubZoneText, IsInInstance;
+local contains, containsValue = app.contains, app.containsValue;
+local wipearray, ArrayAppend, CreateObject, MergeObject, MergeProperties, NestObjects, IsQuestFlaggedCompleted
+	= app.wipearray, app.ArrayAppend, app.__CreateObject, app.MergeObject, app.MergeProperties, app.NestObjects, app.IsQuestFlaggedCompleted
+local GetRelativeGroup, GetRelativeValue, ExpandGroupsRecursively
+	= app.GetRelativeGroup, app.GetRelativeValue, app.ExpandGroupsRecursively;
 
 -- Discord Error Reporting
 local function BuildDiscordMapInfoTable(id, mapInfo)
@@ -67,7 +66,7 @@ local function ShowDiscordReportPopupForMapID(mapID)
 	app.report(app:Linkify(app.Version.." (Click to Report) No data found for this Location!", app.Colors.ChatLinkError, "dialog:" .. popupID));
 end
 
--- Local variables
+-- Classic Style Mini List
 local SortTypeByHeaderID = setmetatable({
 	[app.HeaderConstants.QUESTS] = "ClassicQuestOrder",
 }, {
@@ -300,6 +299,285 @@ local ClassicMapDataStyleMetatable = {
 			end
 		end
 	end
+};
+
+-- Retail Style Mini List
+-- local C_Map_GetMapChildrenInfo = C_Map.GetMapChildrenInfo;
+-- Wraps a given object such that it can act as an unfiltered Header of the base group
+local CreateWrapVisualHeader = app.CreateVisualHeaderWithGroups
+-- Returns the consolidated data format for the next header level
+-- Headers are forced not collectible, and will have their content sorted, and can be copied from the existing Source header
+local function CreateHeaderData(group, header)
+	-- copy an uncollectible version of the existing header
+	if header then
+		-- special case for Difficulty headers, need to be actual difficulty groups to merge properly with any existing
+		if header.difficultyID then
+			header = CreateObject(header, true)
+			header.g = { group }
+			return header
+		end
+		-- special case for Map auto-headers, ignore re-nesting a Map header of the current Map
+		if header.type == "m" and header.keyval == self.mapID then
+			return group
+		end
+		header = CreateWrapVisualHeader(header, {group})
+		header.SortType = "Global"
+		return header
+	else
+		return { g = { group }, ["collectible"] = false, SortType = "Global" };
+	end
+end
+-- set of keys for headers which can be nested in the minilist automatically, but not confined to a direct top header
+local subGroupKeys = {
+	"filterID",
+	"professionID",
+	"raceID",
+	"eventID",
+	"instanceID",
+	"achievementID",
+};
+-- set of keys for headers which can be nested in the minilist within an Instance automatically, but not confined to a direct top header
+local subGroupInstanceKeys = {
+	"filterID",
+	"professionID",
+	"raceID",
+	"eventID",
+	"achievementID",
+};
+-- Headers possible in a hierarchy that should just be ignored
+local ignoredHeaders = app.HeaderData.IGNOREINMINILIST or app.EmptyTable;
+local groups, nested, headerKeys, difficultyGroup, nextParent, headerID, isInInstance
+local rootGroups, mapGroups = {}, {};
+local RetailMapDataStyleMetatable = {
+	__mode = "kv",
+	__index = function(cachedMapData, mapID)
+		local mapData;
+		
+		-- Get all results for this map
+		local results = app.SearchForField("mapID", mapID)
+		if results and #results > 0 then
+			-- I tend to like this way of finding sub-maps, but it does mean we rely on Blizzard and get whatever maps they happen to claim
+			-- are children of a given map... sometimes has weird results like scenarios during quests being considered children in
+			-- other zones. Since it can give us special top-level maps (Anniversary AV) also as children of other top-level maps (Hillsbarad)
+			-- we would need to filter the results and add them properly into the results below via sub-groups if they are maps themselves
+			-- local submapinfos = ArrayAppend(C_Map_GetMapChildrenInfo(mapID, 5), C_Map_GetMapChildrenInfo(mapID, 6))
+			-- if submapinfos then
+				-- for _,mapInfo in ipairs(submapinfos) do
+					-- subresults = app.SearchForField("mapID", mapInfo.mapID)
+					-- app.PrintDebug("Adding Sub-Map Results:",mapInfo.mapID,mapInfo.mapType,#subresults)
+					-- results = ArrayAppend(results, subresults)
+				-- end
+			-- end
+			-- See if there are any sub-maps we should also include by way of the 'maps' field on the 'real' map for this id
+			local rootMap, result
+			for i=1,#results do
+				result = results[i]
+				if result.key == "mapID" and result.mapID == mapID then
+					rootMap = result
+					break;
+				end
+			end
+			local rootMaps = rootMap and rootMap.maps
+			if rootMaps then
+				local subresults, subMapID
+				for i=1,#rootMaps do
+					subMapID = rootMaps[i]
+					if subMapID ~= mapID then
+						subresults = app.SearchForField("mapID", subMapID)
+						-- app.PrintDebug("Adding Sub-Map Results:",subMapID,#subresults)
+						results = ArrayAppend(results, subresults)
+					end
+				end
+			end
+			-- Simplify the returned groups
+			groups = {};
+			wipearray(rootGroups);
+			wipearray(mapGroups);
+			local currentMaps = {[mapID] = true};
+			mapData = { mapID = mapID, g = groups }
+			isInInstance = IsInInstance();
+			headerKeys = isInInstance and subGroupInstanceKeys or subGroupKeys;
+			local group, groupmapID, groupmaps
+			-- split search results by whether they represent the 'root' of the minilist or some other mapped content
+			for i=1,#results do
+				-- do not use any raw Source groups in the final list
+				group = CreateObject(results[i]);
+				groupmapID = group.mapID
+				groupmaps = group.maps
+				-- Instance/Map/Class/Header(of current map) groups are allowed as root of minilist
+				if (group.instanceID or (groupmapID and (group.key == "mapID" or (group.key == "headerID" and groupmapID == mapID))) or group.key == "classID")
+					-- and actually match this minilist...
+					-- only if this group mapID matches the minilist mapID directly or by maps
+					and (groupmapID == mapID or (groupmaps and contains(groupmaps, mapID))) then
+					rootGroups[#rootGroups + 1] = group
+				else
+					mapGroups[#mapGroups + 1] = group
+				end
+			end
+			-- first merge all root groups into the list
+			local groupMaps
+			for i=1,#rootGroups do
+				group = rootGroups[i]
+				groupMaps = group.maps
+				if groupMaps then
+					for i=1,#groupMaps do
+						currentMaps[groupMaps[i]] = true;
+					end
+				end
+				-- app.PrintDebug("Merge as Root",group.hash)
+				MergeProperties(mapData, group, true);
+				NestObjects(mapData, group.g);
+			end
+			local externalMaps = {}
+			-- then merge all mapped groups into the list
+			for i=1,#mapGroups do
+				group = mapGroups[i]
+				-- app.PrintDebug("Mapping:",app:SearchLink(group))
+				nested = nil;
+				difficultyGroup = nil
+
+				-- Get the header chain for the group
+				nextParent = group.parent;
+
+				-- Cache the difficultyGroup, if there is one and we are in an actual instance where the group is being mapped
+				if isInInstance then
+					difficultyGroup = GetRelativeGroup(nextParent, "difficultyID")
+					-- app.PrintDebug("difficultyGroup:",app:SearchLink(difficultyGroup))
+				end
+
+				-- Building the header chain for each mapped Thing
+				while nextParent do
+					headerID = nextParent.headerID
+					if headerID then
+						-- all Headers implicitly are allowed as visual headers in minilist unless explicitly ignored
+						if not ignoredHeaders[headerID] then
+							group = CreateHeaderData(group, nextParent);
+							nested = true;
+						end
+					elseif nextParent.isMinilistHeader then
+						group = CreateHeaderData(group, nextParent);
+						nested = true;
+					else
+						for i=1,#headerKeys do
+							if nextParent[headerKeys[i]] then
+								-- create the specified group Type header
+								group = CreateHeaderData(group, nextParent);
+								nested = true;
+								break;
+							end
+						end
+					end
+					nextParent = nextParent.parent;
+				end
+
+				-- really really special cases...
+				-- Battle Pets get an additional raw Filter nesting
+				if not nested and group.key == "speciesID" then
+					group = app.CreateFilter(101, CreateHeaderData(group));
+				end
+
+				-- If relative to a difficultyGroup, then merge it into one.
+				if difficultyGroup then
+					group = CreateHeaderData(group, difficultyGroup);
+					-- remove the name sorttype from the difficulty-based header
+					group.SortType = nil
+					-- link the difficulty group to the current window header so that it assumes its expected hash
+					group.parent = mapData
+					group.sourceParent = nil
+				end
+
+				-- If we're trying to map in another 'map', nest it into a special group for external maps
+				if group.instanceID or group.mapID then
+					externalMaps[#externalMaps + 1] = group
+					group = nil
+				end
+				if group then
+					-- app.PrintDebug("Merge as Mapped:",app:SearchLink(group))
+					MergeObject(groups, group)
+				end
+			end
+
+			-- Nest our external maps into a special header to reduce minilist header spam
+			if #externalMaps > 0 then
+				local externalMapHeader = app.CreateRawText(TRACKER_FILTER_REMOTE_ZONES, {icon=450908,description=L.REMOTE_ZONES_DESCRIPTION,external=true})
+				externalMapHeader.SortType = "Global";
+				NestObjects(externalMapHeader, externalMaps)
+				MergeObject(groups, externalMapHeader)
+			end
+
+			if #rootGroups == 0 then
+				-- if only one group in the map root, then shift it up as the map root instead
+				local mapG = mapData.g;
+				if #mapG == 1 then
+					local topGroup = mapG[1]
+					if not topGroup.external
+						-- only shift up certain group types
+						and (topGroup.instanceID or topGroup.classID or topGroup.mapID)
+					then
+						mapData.g = nil;
+						-- don't persist the parent links since this will now be a minilist root
+						topGroup.parent = nil
+						topGroup.sourceParent = nil
+						MergeProperties(mapData, topGroup, true);
+						NestObjects(mapData, topGroup.g);
+					end
+				else
+					app.PrintDebug("No root Map groups!",mapID)
+				end
+			end
+
+			mapData.u = nil;
+			mapData.e = nil;
+			if mapData.instanceID then
+				mapData = app.CreateInstance(mapData.instanceID, mapData);
+			else
+				if mapData.classID then
+					mapData = app.CreateCharacterClass(mapData.classID, mapData);
+				else
+					mapData = app.CreateMap(mapData.mapID, mapData);
+				end
+				-- sort top level by name if not in an instance
+				mapData.SortType = "Global";
+			end
+			
+			-- TODO: This is dumb, but apparently its required. (for now?)
+			mapData.visible = true;
+			mapData.back = 1;
+			mapData.indent = 0;
+			
+			-- Cache all of the Current Maps with the same data.
+			for id,_ in pairs(currentMaps) do
+				cachedMapData[id] = mapData;
+			end
+		else
+			-- If we don't have any data cached for this mapID and it exists in game, report it to the chat window.
+			local mapInfo = C_Map_GetMapInfo(mapID);
+			if mapInfo then
+				-- only report for mapIDs which actually exist
+				ShowDiscordReportPopupForMapID(mapID);
+			end
+			mapData = app.CreateMap(mapID, {
+				["name"] = L.MINI_LIST .. " [" .. mapID .. "]",
+				["icon"] = 237385,
+				["description"] = L.MINI_LIST_DESC,
+				["visible"] = true,
+				["g"] = {
+					app.CreateRawText(L.UPDATE_LOCATION_NOW, {
+						["icon"] = 134269,
+						["description"] = L.UPDATE_LOCATION_NOW_DESC,
+						["OnClick"] = function(row, button)
+							app.LocationTrigger();
+							return true;
+						end,
+						["OnUpdate"] = app.AlwaysShowUpdate,
+					}),
+				},
+			});
+			cachedMapData[mapID] = mapData;
+		end
+		app.AssignChildren(mapData);
+		return mapData;
+	end,
 };
 
 -- Shared Mini List Behaviours
