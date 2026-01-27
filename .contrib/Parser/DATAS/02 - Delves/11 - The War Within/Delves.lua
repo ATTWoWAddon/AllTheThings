4f0ecdbb77ab270af4255dc08d6895be67125d06
Dmitry Volodin <mr.molkree@gmail.com>
2026-01-28 00:40:14 +0400

    Tiny formatting fix

--- .contrib/Parser/DATAS/02 - Delves/11 - The War Within/Delves.lua
+++ .contrib/Parser/DATAS/02 - Delves/11 - The War Within/Delves.lua
@@ -276,7 +276,7 @@ root(ROOTS.Delves, expansion(EXPANSION.TWW, applyDataSelf({ ["timeline"] = { ADD
 						i(238182),	-- Delver's Mana-Skimmer Schematic: Hyperdrive (MM!)
 					})),
 					ach(42193, {	-- My Stab-Happy Nemesis
-						i(245529, {		-- Hal'hadar's Phasebound Visor (COSMETIC!)
+						i(245529, {	-- Hal'hadar's Phasebound Visor (COSMETIC!)
 							["timeline"] = { REMOVED_12_0_0 },
 						}),
 					}),
