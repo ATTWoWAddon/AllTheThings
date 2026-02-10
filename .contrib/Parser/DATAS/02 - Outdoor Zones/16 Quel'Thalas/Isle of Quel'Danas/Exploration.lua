c5c034aacf6e0bf2e60eead370b606722b1fc137
Crieve <dylanfortune@live.com>
2026-02-09 21:55:52 -0600

    Added some exploration nodes for Quel'Thalas.

--- .contrib/Parser/DATAS/02 - Outdoor Zones/16 Quel'Thalas/Isle of Quel'Danas/Exploration.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/16 Quel'Thalas/Isle of Quel'Danas/Exploration.lua
@@ -5,6 +5,10 @@
 root(ROOTS.Zones, m(MAP.MIDNIGHT.QUELTHALAS, {
 	m(MAP.MIDNIGHT.ISLE_OF_QUELDANAS, {
 		explorationHeader({
+			exploration(16754),    -- Court of the Phoenix
+			exploration(16815),    -- Greengill Coast
+			exploration(16215),    -- Isle of Quel'Danas
+			exploration(16526),    -- The Sunwell
 		}),
 	}),
 }));
--- .contrib/Parser/DATAS/02 - Outdoor Zones/16 Quel'Thalas/Zul'Aman/Exploration.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/16 Quel'Thalas/Zul'Aman/Exploration.lua
@@ -5,6 +5,10 @@
 root(ROOTS.Zones, m(MAP.MIDNIGHT.QUELTHALAS, {
 	m(MAP.MIDNIGHT.ZULAMAN, {
 		explorationHeader({
+			exploration(16052),    -- Amani Pass
+			exploration(16194),    -- Atal'Aman
+			exploration(16383),    -- Swiftclaw Terrace
+			exploration(16382),    -- Talonstrike Terrace
 		}),
 	}),
 }));
