ffb9577a9f75dcf852a10dcd6aa84b27b77c5c16
Dmitry Volodin <mr.molkree@gmail.com>
2026-02-11 13:20:46 +0400

    Mark Piping Hot Portable Bakery as removed

--- .contrib/Parser/DATAS/12 - Promotions/Discord.lua
+++ .contrib/Parser/DATAS/12 - Promotions/Discord.lua
@@ -36,7 +36,7 @@ root(ROOTS.Promotions, n(DISCORD_PROMOTION, {
 		}),
 		i(250292, {	-- Piping Hot Portable Bakery (COSMETIC!)
 			["description"] = "At the top of your Direct messages tab on Discord, click the Quests tab to start the Quest for the Reward.\n\nPlay the game for 15 minutes with Discord running.\n\nUpon completion of the quest, you'll be given a code to redeem - head to the Battle.net launcher, click your profile in the top right, and choose 'Redeem Code.'\n\nPromotion is available from January 27th, 2026 until February 2nd, 2026 (11:59PM UTC).",
-			["timeline"] = { ADDED_12_0_0, "removed 12.0.0.99999" },
+			["timeline"] = { ADDED_12_0_0, "removed 12.0.0.65655" },
 		}),
 	},
 }));
