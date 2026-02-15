79e998fd8721a170421ca35586706fd720485dfa
Darkal <michalbert@seznam.cz>
2026-02-15 20:22:17 +0100

    Moved NYI objects into its own file.

---
+++ .contrib/Parser/DATAS/17 - NYI/NYI Objects.lua
@@ -0,0 +1,17 @@
+---------------------------------------------------------------
+--     N E V E R    I M P L E M E N T E D    M O D U L E     --
+---------------------------------------------------------------
+root(ROOTS.NeverImplemented, n(TREASURES, {
+	expansion(EXPANSION.MID, {
+		-- 12.0.0
+		expansion(EXPANSION.MID, patch(0,0,1), bubbleDownSelf({ ["timeline"] = { CREATED_12_0_0 } }, {
+			-- Objects referenced by achievement 61344 (Chronicler of the Haranir) which parser complains about, wowhead doesn't know them
+			o(579180),	-- ???
+			o(579182),	-- ???
+			o(579183),	-- ???
+			o(579201),	-- ???
+			o(579202),	-- ???
+			o(579203),	-- ???
+		})),
+	}),
+}));
