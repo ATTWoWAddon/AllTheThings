91baaa5898d012a4c561614eeda5d25d88d40143
milotic <al0305@hotmail.com>
2026-02-16 17:12:19 -0600

    [Classic] The bosses from the elemental invasions have been moved from rares to their own file, Removed redundant note of cooking because tooltip already display the info (#2302)
    * Removed redundant note of cooking, the tooltip is already tracking this info
    
    * [Classic] The bosses from the elemental invasions have been moved from rares to their own file

--- .contrib/Parser/DATAS/02 - Outdoor Zones/01 Kalimdor/Un'goro Crater.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/01 Kalimdor/Un'goro Crater.lua
@@ -2096,32 +2096,6 @@ root(ROOTS.Zones, m(KALIMDOR, {
 				}),
 			}),
 			n(RARES, {
-				n(14461, {	-- Baron Charr
-					-- TODO: Move this to an Elemental Invasion World Event file.
-					["description"] = "This is only available during an Elemental Invasion.",
-					["coords"] = {
-						{ 44.6, 46.0, UNGORO_CRATER },
-						{ 48.2, 41.2, UNGORO_CRATER },
-						{ 53.2, 41.8, UNGORO_CRATER },
-						{ 56.6, 42.8, UNGORO_CRATER },
-						{ 56.6, 48.2, UNGORO_CRATER },
-						{ 55.6, 57.4, UNGORO_CRATER },
-						{ 51.6, 57.6, UNGORO_CRATER },
-						{ 45.4, 54.8, UNGORO_CRATER },
-					},
-					["timeline"] = { REMOVED_4_0_3 },
-					["groups"] = {
-						i(18671, {	-- Baron Charr's Sceptre
-							["timeline"] = { REMOVED_4_0_3 },
-						}),
-						i(18672, {	-- Elemental Ember
-							["timeline"] = { REMOVED_4_0_3 },
-						}),
-						-- #if BEFORE 4.0.3
-						applyclassicphase(PHASE_THREE_DMF_CARDS, i(19268)),	-- Ace of Elementals
-						-- #endif
-					},
-				}),
 				n(6582, {	-- Clutchmother Zavas
 					["coords"] = {
 						-- #if AFTER CATA
