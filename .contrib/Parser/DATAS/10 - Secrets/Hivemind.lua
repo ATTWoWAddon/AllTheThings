b5b52a49d35d9d414c832b956014d50b1dd83739
Roman Pawel <r.pawel91@hotmail.com>
2026-02-18 09:31:22 +0100

    Fix location reference in Hivemind.lua description

--- .contrib/Parser/DATAS/10 - Secrets/Hivemind.lua
+++ .contrib/Parser/DATAS/10 - Secrets/Hivemind.lua
@@ -44,7 +44,7 @@ root(ROOTS.Secrets, {
 								["questID"] = 40404,
 								["coord"] = { 47.4, 75.0, DEADWIND_PASS },	-- Karazhan
 								["sourceQuest"] = 40314,	-- Leter 2: Prepfoot Compound, Highmountain
-								["description"] = "|cFFFFFFFFLetter 3:|r Go to |cFFFFD700Karazhan (Old)|r in |cFFFFD700Duskwood|r. The third letter is located in |cFFFFD700Medivh's Chambers|r, located after the Chess Event, in the staircase leading to Prince Malchezaar. The letter is on the chair Medivh used to write his scrolls and spells, literally the seat of the guardian. Click it. The letter reads...\n\n|cFFFFFFFFI sat Dumbfounded, watching As the most Subtle Rat reached for the cheese a third time in under an hour.|r\n"
+								["description"] = "|cFFFFFFFFLetter 3:|r Go to |cFFFFD700Karazhan (Old)|r in |cFFFFD700Deadwind Pass|r. The third letter is located in |cFFFFD700Medivh's Chambers|r, located after the Chess Event, in the staircase leading to Prince Malchezaar. The letter is on the chair Medivh used to write his scrolls and spells, literally the seat of the guardian. Click it. The letter reads...\n\n|cFFFFFFFFI sat Dumbfounded, watching As the most Subtle Rat reached for the cheese a third time in under an hour.|r\n"
 							}),
 							o(280838, {	-- Letter 4: Razorfen Downs
 								["model"] = 1661948,
