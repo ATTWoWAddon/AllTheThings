edd633e425b395017712a251770cdc7648ebd320
Milotic <al0305@hotmail.com>
2026-04-07 13:24:51 -0600

    Retail-Anniversary TBC Echoes of midnight- Voidfeather Dragonhawk Promo

--- .contrib/Parser/DATAS/02 - Outdoor Zones/16 Quel'Thalas/Isle of Quel'Danas/Achievements.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/16 Quel'Thalas/Isle of Quel'Danas/Achievements.lua
@@ -6,7 +6,9 @@ root(ROOTS.Zones, m(MAP.MIDNIGHT.QUELTHALAS, {
 	m(MAP.MIDNIGHT.ISLE_OF_QUELDANAS, {
 		n(ACHIEVEMENTS, {
 			ach(62191),	-- Call of the Light
-			ach(62273),	-- Echoes of Midnight
+			ach(62273, {	-- Echoes of Midnight
+				["description"] = "Earn the menacing Voidfeather Dragonhawk flying mount in Burning Crusade Classic Anniversary Edition", -- Timeout Correctly ends: Available until May 16, 2026, 10:00 am PDT (18:00 BST). https://worldofwarcraft.blizzard.com/en-us/news/24267942
+			}),
 		}),
 	}),
 }));
