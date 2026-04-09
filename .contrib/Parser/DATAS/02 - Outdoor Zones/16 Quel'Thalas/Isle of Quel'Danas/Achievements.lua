4809a9b01e50880e55e6b4430f02555e94d114a5
Exodius <Exodius@users.noreply.github.com>
2026-04-09 16:54:10 +0200

    Modify 'Echoes of Midnight' achievement details

--- .contrib/Parser/DATAS/02 - Outdoor Zones/16 Quel'Thalas/Isle of Quel'Danas/Achievements.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/16 Quel'Thalas/Isle of Quel'Danas/Achievements.lua
@@ -6,8 +6,8 @@ root(ROOTS.Zones, m(MAP.MIDNIGHT.QUELTHALAS, {
 	m(MAP.MIDNIGHT.ISLE_OF_QUELDANAS, {
 		n(ACHIEVEMENTS, {
 			ach(62191),	-- Call of the Light
-			ach(62273, {	-- Echoes of Midnight
-				["description"] = "Earn the menacing Voidfeather Dragonhawk flying mount in Burning Crusade Classic Anniversary Edition", -- Timeout Correctly ends: Available until May 16, 2026, 10:00 am PDT (18:00 BST). https://worldofwarcraft.blizzard.com/en-us/news/24267942
+			ach(62273, {	-- Echoes of Midnight	// https://worldofwarcraft.blizzard.com/en-us/news/24267942
+				["description"] = "Earning this achievement rewards a 'Voidfeather Dragonhawk' flying mount on Anniversary realms if completed before May 16th, 2026.",	-- Add preprocessors if required
 			}),
 		}),
 	}),
