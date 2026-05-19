b0f28b3d5ec568eb00f5e2ba92ee1223a2f53c38
Milotic <al0305@hotmail.com>
2026-05-19 10:42:30 -0600

    [Anniversary] Timeline phase 2 and the promo with retail was removed

--- .contrib/Parser/lib/Constants/Timelines.lua
+++ .contrib/Parser/lib/Constants/Timelines.lua
@@ -290,11 +290,11 @@ REMOVED_2_5_4_A = "removed 2.5.4.43638";				-- The Burning Crusade Classic 2.5.4
 CREATED_2_5_5 = "created 2.5.5.65340";					-- The Burning Crusade Classic Pre-patch launch on January 13th, 2026
 ADDED_2_5_5 = "added 2.5.5.65340";						-- The Burning Crusade Classic Pre-patch launch on January 13th, 2026
 REMOVED_2_5_5 = "removed 2.5.5.65340";					-- The Burning Crusade Classic Pre-patch launch on January 13th, 2026
-CREATED_2_5_6 = "created 2.5.6";						-- CRIEVE NOTE: Sorry, in a rush. Update this when it happens. TBC Phase 2.
-ADDED_2_5_6 = "added 2.5.6";
-REMOVED_2_5_6 = "removed 2.5.6";
+CREATED_2_5_5_PHASE_2 = "created 2.5.5.67511";			-- The Burning Crusade Classic 2.5.5 phase 2 launch on May 14th, 2026
+ADDED_2_5_5_PHASE_2 = "added 2.5.5.67511";				-- The Burning Crusade Classic 2.5.5 phase 2 launch on May 14th, 2026
+REMOVED_2_5_5_PHASE_2 = "removed 2.5.5.67511";			-- The Burning Crusade Classic 2.5.5 phase 2 launch on May 14th, 2026
 --- DYNAMIC CLASSIC TIMELINES ---
-REMOVED_TBC_PHASE_TWO = REMOVED_2_5_6;
+REMOVED_TBC_PHASE_TWO = REMOVED_2_5_5_PHASE_2;
 
 ----------------------------
 -- WRATH OF THE LICH KING --
