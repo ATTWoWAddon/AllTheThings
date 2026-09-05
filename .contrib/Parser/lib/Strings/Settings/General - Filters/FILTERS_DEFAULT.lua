f3fa187497df5d58cf1628cae6f12f3c95fbada3
Dmitry Volodin <mr.molkree@gmail.com>
2026-09-05 18:54:27 +0400

    Use DEFAULT locale global constant

--- .contrib/Parser/lib/Strings/Settings/General - Filters/FILTERS_DEFAULT.lua
+++ .contrib/Parser/lib/Strings/Settings/General - Filters/FILTERS_DEFAULT.lua
@@ -3,16 +3,6 @@ createLocalizationString({
 	constant = "FILTERS_DEFAULT",
 	export = true,
 	text = {
-		en = "Default",
-		-- TODO: de = "",
-		-- TODO: es = "",
-		-- TODO: mx = "",
-		-- TODO: fr = "",
-		-- TODO: it = "",
-		-- TODO: pt = "",
-		-- TODO: ru = "",
-		-- TODO: ko = "",
-		-- TODO: cn = "",
-		-- TODO: tw = "",
+		en = [[~DEFAULT]],
 	},
 })
