528a3f1f1c3e6590e4547539fa34b449c071c178
Tyler Akins <tyler.akins@gocrisp.com>
2026-09-25 11:36:33 -0500

    [FOREVER] Complete Zephras Isle quest data

--- .contrib/.db/forever/zzOLD/02 - Outdoor Zones/01 Kalimdor/2 - Orgrimmar.lua
+++ .contrib/.db/forever/zzOLD/02 - Outdoor Zones/01 Kalimdor/2 - Orgrimmar.lua
@@ -372,6 +372,13 @@ root(ROOTS.Zones, m(MAP.KALIMDOR, {
 						},
 					},
 				}),
+				q(93739, {	-- Exploring the Horde
+					["sourceQuest"] = 95350,	-- Welcome to Azeroth
+					["qg"] = 4949,	-- Thrall <Warchief>
+					["coord"] = { 32.0, 37.8, MAP.ORGRIMMAR },
+					["races"] = { SKYBORNE_HORDE },
+					["lvl"] = 7,
+				}),
 				q(5726, {	-- Hidden Enemies (1/5)
 					["qg"] = 4949,	-- Thrall <Warchief>
 					["coord"] = { 32.0, 37.8, MAP.ORGRIMMAR },
