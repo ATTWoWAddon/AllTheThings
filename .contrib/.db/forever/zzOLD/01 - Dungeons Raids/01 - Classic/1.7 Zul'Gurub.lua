23df77e8f7ab38b7754e947e39a4893c722de4f4
Milotic <al0305@hotmail.com>
2026-09-26 13:31:57 -0600

    [Vanilla] Fixed A Collection of Heads (8201)

--- .contrib/.db/forever/zzOLD/01 - Dungeons Raids/01 - Classic/1.7 Zul'Gurub.lua
+++ .contrib/.db/forever/zzOLD/01 - Dungeons Raids/01 - Classic/1.7 Zul'Gurub.lua
@@ -586,6 +586,7 @@ root(ROOTS.Instances, expansion(EXPANSION.CLASSIC, applyclassicphase(PHASE_FOUR,
 				}),
 				q(8201, {	-- A Collection of Heads
 					["qg"] = 14910,	-- Exzhal <Servitor of Rastakhan>
+					["qi"] = 19883,	-- Sacred Cord (QI!)
 					["coord"] = { 15.3, 15.5, MAP.STRANGLETHORN_VALE },
 					["lvl"] = 58,
 					["groups"] = {
@@ -1907,6 +1908,7 @@ root(ROOTS.Instances, expansion(EXPANSION.CLASSIC, applyclassicphase(PHASE_FOUR,
 					}),
 					q(8201, {	-- A Collection of Heads
 						["qg"] = 14910,	-- Exzhal <Servitor of Rastakhan>
+						["qi"] = 19883,	-- Sacred Cord (QI!)
 						["coord"] = { 15.3, 15.5, MAP.STRANGLETHORN_VALE },
 						["lvl"] = 58,
 						["groups"] = {
